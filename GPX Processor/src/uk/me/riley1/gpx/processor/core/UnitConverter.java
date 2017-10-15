7f42dd01b00962dc6b2428de24c444f605e53da3
gerryriley <gerry@riley1.me.uk>
2017-10-15 23:49:11 +0100

    More distance unit plurality changes

--- GPX Processor/src/uk/me/riley1/gpx/processor/core/UnitConverter.java
+++ GPX Processor/src/uk/me/riley1/gpx/processor/core/UnitConverter.java
@@ -1,7 +1,7 @@
 package uk.me.riley1.gpx.processor.core;
 
 public enum UnitConverter {
-	 MILES("Mile", 0.000621f), KM("Kilometer", 0.001f), METERS("Meters", 1), FEET("Feet",3.28084f ),
+	 MILES("Mile", 0.000621f), KM("Kilometer", 0.001f), METERS("Meter", 1), FEET("Feet",3.28084f ),
 	 YARDS("Yard", 1.093613f), CM("Centimeter", 100), MM("Millimeter", 1000);
 	
 	private float convFactor;
